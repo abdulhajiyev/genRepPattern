ccfe874d3e98c0a298ed18a4d7fe2bc97212b0d9
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-12-03 21:26:21 +0400

    hakuna

---
+++ genRepPattern/DataAccess/Concrete/GenericRepositoryPattern.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using genRepPattern.DataAccess.Abstract;
+using genRepPattern.DataAccess.Context;
+
+namespace genRepPattern.DataAccess.Concrete
+{
+    public class GenericRepositoryPattern<T> : IGenericRepositoryPattern<T> where T : class
+    {
+        protected DataClassesDataContext _context;
+
+        public GenericRepositoryPattern()
+        {
+            _context = new DataClassesDataContext();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+           return from entity in _context.GetTable<T>() select entity;
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>> exp)
+        {
+           return _context.GetTable<T>().Where(exp);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.GetTable<T>().InsertOnSubmit(entity);
+            SubmitChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.GetTable<T>().DeleteOnSubmit(entity);
+            SubmitChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.GetTable<T>().Attach(entity);
+            SubmitChanges();
+        }
+
+        public void SubmitChanges()
+        {
+            _context.SubmitChanges();
+        }
+
+    }
+}
--- genRepPattern/MainWindow.xaml.cs
+++ genRepPattern/MainWindow.xaml.cs
@@ -1,28 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using genRepPattern.DataAccess.Abstract;
+using genRepPattern.DataAccess.Concrete;
+using genRepPattern.DataAccess.Context;
 
 namespace genRepPattern
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        GenericRepositoryPattern<Author> _repository;
+        public ObservableCollection<Author> Data { get; set; }
+
+
+        public Author SelectedItem
+        {
+            get { return (Author)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(Author), typeof(MainWindow));
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            _repository = new GenericRepositoryPattern<Author>();
+            Data = new ObservableCollection<Author>(_repository.GetAll().ToList());
+            ListView.DisplayMemberPath = "FirstName";
+
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            _repository.SubmitChanges();
+        }
+
+        private void Insert_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = _repository.GetAll().OrderBy(a => a.Id).ToList();
+
+            var window1 = new Window1();
+            window1.ShowDialog();
+
+            var auth = new Author
+            {
+                FirstName = window1.FirstName,
+                LastName = window1.LastName,
+                Id = temp[temp.Count - 1].Id + 1
+            };
+            Data.Add(auth);
+            _repository.SubmitChanges();
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            Data.Remove(SelectedItem);
+            _repository.SubmitChanges();
         }
     }
 }
